ba02517475eaa1a27aa5f7fd893cbb1313bee7de
Simon Bull <simon.bull.84@googlemail.com>
2017-12-21 09:18:53 +0000

    Logging in auth callback

--- src/routes.js
+++ src/routes.js
@@ -2,21 +2,26 @@ const passport = require('passport');
 const profile = require('./app/profile');
 const changePassword = require('./app/changePassword');
 const signOut = require('./app/signOut');
+const logger = require('./infrastructure/logger');
 
 const routes = (app, csrf) => {
   // auth callbacks
   app.get('/auth', passport.authenticate('oidc'));
   app.get('/auth/cb', (req, res, next) => {
+    logger.info('in auth callback');
+
     passport.authenticate('oidc', (err, user) => {
       let redirectUrl = '/';
 
       if (err) {
+        logger.error(`Error in auth callback - ${err}`);
         return next(err);
       }
       if (!user) {
         return res.redirect('/');
       }
 
+      logger.info(`Authenticated user ${JSON.stringify(user)}`);
       if (req.session.redirectUrl) {
         redirectUrl = req.session.redirectUrl;
         req.session.redirectUrl = null;
@@ -24,6 +29,7 @@ const routes = (app, csrf) => {
 
       return req.logIn(user, (loginErr) => {
         if (loginErr) {
+          logger.error(`Login error in auth callback - ${loginErr}`);
           return next(loginErr);
         }
         if (redirectUrl.endsWith('signout/complete')) redirectUrl = '/';
