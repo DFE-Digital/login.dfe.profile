02a1c2a429a980b0a0e1025a2122a91d4edad991
James King <hello@jamesmichaelking.com>
2018-02-26 09:49:42 +0000

    Added user info to change password view

--- src/app/changePassword/get.js
+++ src/app/changePassword/get.js
@@ -1,7 +1,11 @@
+const { getUserDisplayName } = require('../../infrastructure/utils');
+
 const action = (req, res) => {
   res.render('changePassword/views/change', {
     csrfToken: req.csrfToken(),
     title: 'Change password',
+    displayName: req.user ? getUserDisplayName(req.user) : 'Unknown User',
+    user: req.user,
     validationFailed: false,
     validationMessages: {},
   });
