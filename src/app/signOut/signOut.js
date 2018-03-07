3bc315b783f54e2bc42f4bb352ad867a19169bf1
Simon Bull <simon.bull.84@googlemail.com>
2018-03-07 07:48:18 +0000

    Allow signout redirect uri to be passed in (for SAML proxy)

--- src/app/signOut/signOut.js
+++ src/app/signOut/signOut.js
@@ -7,10 +7,13 @@ const passport = require('passport');
 const config = require('./../../infrastructure/config');
 
 const signUserOut = (req, res) => {
-  if (req.user.id_token) {
+  if (req.user && req.user.id_token) {
     const idToken = req.user.id_token;
     const issuer = passport._strategies.oidc._issuer;
-    const returnUrl = `${config.hostingEnvironment.protocol}://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}/signout/complete`;
+    let returnUrl = `${config.hostingEnvironment.protocol}://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}/signout/complete`;
+    if (req.query.redirect_uri) {
+      returnUrl = req.query.redirect_uri;
+    }
     req.logout();
     res.redirect(url.format(Object.assign(url.parse(issuer.end_session_endpoint), {
       search: null,
@@ -20,7 +23,7 @@ const signUserOut = (req, res) => {
       },
     })));
   } else {
-    res.redirect('/');
+    res.redirect(req.query.redirect_uri ? req.query.redirect_uri : '/');
   }
 };
 
