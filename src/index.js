8f995a2db58ead77ea6800934d6893a4b0805fcf
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-04-02 16:27:18 +0100

    docker setup

--- src/index.js
+++ src/index.js
@@ -132,7 +132,7 @@ const init = async () => {
     errorPageRenderer,
   }));
 
-  if (config.hostingEnvironment.env === 'dev' || config.hostingEnvironment.env === 'docker') {
+  if (config.hostingEnvironment.env === 'dev') {
     app.proxy = true;
 
     const options = {
@@ -147,6 +147,7 @@ const init = async () => {
       logger.info(`Dev server listening on https://${config.hostingEnvironment.host}:${config.hostingEnvironment.port} with config:\n${JSON.stringify(config)}`);
     });
   } else if (config.hostingEnvironment.env === 'docker') {
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
     app.listen(config.hostingEnvironment.port, () => {
       logger.info(`Server listening on http://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}`)
     });
