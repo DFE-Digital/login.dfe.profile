eadafaaf4067ee07a915b60b60c313c3afac7779
Floyd Price <f.price@floydprice.com>
2019-07-10 21:10:17 +0100

    call user info endpoint to enrich user claims

--- src/infrastructure/oidc/index.js
+++ src/infrastructure/oidc/index.js
@@ -20,7 +20,18 @@ const getPassportStrategy = async () => {
     client,
     params: { redirect_uri: `${config.hostingEnvironment.protocol}://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}/auth/cb`, scope: 'openid profile email' },
   }, (tokenset, authUserInfo, done) => {
-    done(null, { ...tokenset.claims, id_token: tokenset.id_token, id: tokenset.claims.sub, name: tokenset.sub });
+    client.userinfo(tokenset.access_token)
+      .then((userInfo) => {
+        userInfo.id = userInfo.sub;
+        userInfo.name = userInfo.sub;
+        userInfo.id_token = tokenset.id_token;
+
+        done(null, userInfo);
+      })
+      .catch((err) => {
+        logger.error(err);
+        done(err);
+      });
   });
 };
 
